5859b0f6a747e1493aa3a5cda4d7a64434349f48
Ramon Vermeulen <ramon.r.vermeulen@student.hu.nl>
2018-06-14 23:32:33 +0200

    Some small styling changes and default value for the direction component

--- src/app/components/generator/direction/direction.component.ts
+++ src/app/components/generator/direction/direction.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'direction-component',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./direction.component.css']
 })
 export class DirectionComponent implements OnInit {
-    public direction;
+    public direction = "top";
 
     constructor() { }
 
--- src/app/components/generator/generator.component.ts
+++ src/app/components/generator/generator.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'generator-component',
-  templateUrl: './generator.component.html',
-  styleUrls: ['./generator.component.css']
+    selector: 'generator-component',
+    templateUrl: './generator.component.html',
+    styleUrls: ['./generator.component.css']
 })
 export class GeneratorComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
