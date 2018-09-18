a3aab763995b1923f09a28e1e4cf1add0b2f14d4
Ramon Vermeulen <ramon.r.vermeulen@student.hu.nl>
2018-09-18 09:51:57 +0200

    Implementation of 'copy to clipboard'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 
+
+/* external library imports */
+import { ColorPickerModule } from 'ngx-color-picker';
+import { ClipboardModule } from 'ngx-clipboard';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { GeneratorComponent } from './components/generator/generator.component';
 import { DirectionComponent } from './components/generator/direction/direction.component';
 import { TypeComponent } from './components/generator/type/type.component';
 import { DimensionsComponent } from './components/generator/dimensions/dimensions.component';
-
-/* external library imports */
-import { ColorPickerModule } from 'ngx-color-picker';
 import { ColorComponent } from './components/generator/color/color.component';
 import { VisualPreviewComponent } from './components/generator/visual-preview/visual-preview.component';
 import { CodePreviewComponent } from './components/generator/code-preview/code-preview.component';
@@ -32,7 +34,8 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     FormsModule,
-    ColorPickerModule
+    ColorPickerModule,
+    ClipboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
